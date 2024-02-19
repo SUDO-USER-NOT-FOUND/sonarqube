71ed09a3c30fb15cfecdbb34f1fdee75113cbbbd
Your Name <you@example.com>
2024-02-19 05:53:43 +0000

    Jenkins file updated

--- import.sh
+++ import.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Directory containing your .sql files 
-SQL_FILES_DIR="/var/lib/jenkins/workspace/jenkins-github_sonarqube_main/Database"
+SQL_FILES_DIR="/var/lib/jenkins/workspace/jenkins-github_sonarqube_main/database"
 
 # Temporary directory for intermediate files
 TMP_DIR="$SQL_FILES_DIR/tmp"
